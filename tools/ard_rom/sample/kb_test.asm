0e8d74e9215fc13dc5f289f193e7fc1eeaa8f9c7
Daniel Domurad <ddomurad@gmail.com>
2024-01-03 08:16:34 +0100

    Updates

--- tools/ard_rom/sample/kb_test.asm
+++ tools/ard_rom/sample/kb_test.asm
@@ -1,36 +1,62 @@
+; Simple test of the numeric keyboard 
+; and LCD display 
 
+; ---------------------------------------
+;   Definitions 
+; ---------------------------------------
 
-.org 0xf000 
-  ldai 0b00001100 ; enable
-  sta 0x8000
-  ldai 0x80       ; set DDRAM = 0x00
-  sta 0x8000 
+.def (
+  ZPAGE := 0x0000
+  RST_VTR := 0xfffc
+  ENTER   := 0xf000
 
-  ldai 0x01       ; clear display
-  sta 0x8000 
-  ldai 0x00
+  LCD_CMD_ADDR   := 0x8000 
+  LCD_DATA_ADDR  := 0x8001 
+
+  LCD_CMD_ENABLE := 0b00001100
+  LCD_CMD_RSET_DDRAM := 0x80
+  LCD_CMD_CLR_DISPLAY := 0x01
+
+  KBR_ADDR := 0xc000
+)
+
+.org ZPAGE
+key_buffer: .byte 1
+
+; ---------------------------------------
+;   Routine 
+; ---------------------------------------
+.org ENTER 
+  ldai  LCD_CMD_ENABLE     ; enable
+  sta LCD_CMD_ADDR 
+  ldai LCD_CMD_RSET_DDRAM  ; set DDRAM = 0x00
+  sta LCD_CMD_ADDR 
+  ldai LCD_CMD_CLR_DISPLAY ; clear display
+  sta LCD_CMD_ADDR 
+
+  ldai key_buffer
 inf_loop:
   jsr read_key
   clc
-  lda 0x00 
-  beq inf_loop
-  sta 0x8001
+  lda key_buffer 
+  bne inf_loop
+  sta LCD_DATA_ADDR
   bra inf_loop
 
 read_key:
   clc
-  lda 0xc000
+  lda KBR_ADDR + 0
   bne store_key_and_return
-  lda 0xc001
+  lda KBR_ADDR + 1
   bne store_key_and_return
-  lda 0xc002
+  lda KBR_ADDR + 2
   bne store_key_and_return
-  lda 0xc003
+  lda KBR_ADDR + 3
   bne store_key_and_return
 store_key_and_return:
-  sta 0x00
+  sta key_buffer
   rts 
   
 
-.org 0xfffc
-.db 0x00, 0xf0
+.org RST_VTR
+.db ENTER.l, ENTER.h
